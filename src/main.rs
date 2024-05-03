b4f8c5c8478d4d23dc28c80935548acc0647a18a
light-planck <seseifunc16@gmail.com>
2024-05-03 09:38:50 +0900

    feat: echo機能を追加

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use clap::Parser;
+
+/// Rust echo
+#[derive(Parser, Debug)]
+#[command(author, version, about)]
+struct Args {
+    /// Input text
+    #[arg(required(true))]
+    text: Vec<String>,
+
+    /// Do not print newline
+    #[arg(short('n'))]
+    omit_new_line: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    print!(
+        "{}{}",
+        args.text.join(" "),
+        if args.omit_new_line { "" } else { "\n" }
+    );
 }
